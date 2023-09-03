d40ce4a7c7eb404b7e45255b444afe9bb88b0cef
JHLM <josehenriquelm@gmail.com>
2023-09-03 19:20:19 -0300

    Adds gouraud interpolation as a shading option

--- src/shader_fragment.glsl
+++ src/shader_fragment.glsl
@@ -61,138 +61,149 @@ out vec4 color;
 #define PHONG 1
 #define BLINN_PHONG 2
 
+// Define se usamos a interpolação de gouraud
+uniform bool interp_Gouraud;
+
+// Cor obtida por interp de gouraud (vertex shader)
+in vec4 color_interp;
+
 
 void main()
 {
-    // Coordenadas de textura U e V
-    float U = 0.0;
-    float V = 0.0;
-
-    // Aqui usamos o tipo do objeto. Possivelmente podemos ir passando
-    // outros atributos para o shader, como por exemplo o tipo de interpolação de iluminação
-
-    // Objetos genéricos (falta de um nome melhor) usam as coordenadas
-    // de textura do objeto
-    if ( object_type == GENERIC_OBJECT){
-        U = texcoords.x;
-        V = texcoords.y;
-    }
-    // Meio que um placeholder, mas mapeia textura com coordenadas esféricas
-    else if ( object_type == SPHERICAL_OBJECT){
-
-        // Cálculo de texturas com coordenadas esféricas
-        vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
-        vec4 p_vec = position_model - bbox_center;
-        vec4 p_hat = bbox_center + (p_vec / length(p_vec));
-        
-        float phi = asin(p_hat[1]);
-        float theta = atan(p_hat[0],p_hat[2]);
-        U = (theta + M_PI) / (2 * M_PI);
-        V = (phi + M_PI_2) / M_PI;
-    }
-
-    // Obtemos a posição da câmera utilizando a inversa da matriz que define o
-    // sistema de coordenadas da câmera.
-        // Obtemos a posição da câmera utilizando a inversa da matriz que define o
-    // sistema de coordenadas da câmera.
-    vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
-    vec4 camera_position = inverse(view) * origin;
-
-    // O fragmento atual é coberto por um ponto que percente à superfície de um
-    // dos objetos virtuais da cena. Este ponto, p, possui uma posição no
-    // sistema de coordenadas global (World coordinates). Esta posição é obtida
-    // através da interpolação, feita pelo rasterizador, da posição de cada
-    // vértice.
-    vec4 p = position_world;
-
-    // Normal do fragmento atual, interpolada pelo rasterizador a partir das
-    // normais de cada vértice.
-    vec4 n = normalize(normal);    
-
-    // Vetor que define o sentido da câmera em relação ao ponto atual.
-    vec4 v = normalize(camera_position - p);
-
-    // Posição da fonte de luz
-    vec4 l = light_position - p;
-    
-    const float epsilon = 1e-5;    
-    float cos_beta = dot(normalize(light_direction), normalize(p - light_position));
-    float cos_alpha = cos(light_aperture_angle);
-
-    /*
-        MODELOS DE ILUMINAÇÃO
-    */
-
-    vec3 Kd = Kd0 * texture(diffMap, vec2(U,V) * object_texture_scale).rgb;
-    vec3 Ka = Ka0;
-    vec3 Ks = Ks0; // Refletância especular do objeto
-    float q = ns; // Expoente especular para o modelo de iluminação de Phong
-    vec3 final_color;
-    
-    // Espectro da fonte de iluminação
-    vec3 I = vec3(1.0,1.0,1.0); 
-    
-    // ILUMINAÇÃO DE LAMBERT (DIFUSA)
-    // Termo difuso de Lambert
-    vec3 lambert_diffuse_term = Kd * I * max(0, dot(n, l)); 
-
-    // Equação da iluminação de lambert
-    vec3 lambert_shading = lambert_diffuse_term;
-
-    // ILUMINAÇÃO DE PHONG 
-    // Espectro da luz ambiente
-    vec3 Ia = vec3(0.2,0.2,0.2);
-    // Termo ambiente 
-    vec3 ambient_term = Ka * Ia; 
-    // Vetor que define o sentido da reflexão especular ideal.
-    vec4 r = -l + 2 * n * dot(n, l);
-    // Termo especular utilizando o modelo de iluminação de Phong
-    vec3 phong_specular_term  = Ks * I * pow(max(0, dot(r, v)), q); 
-
-    // Equação da iluminação de Phong
-    vec3 phong_shading = lambert_diffuse_term + ambient_term + phong_specular_term;
-
-    // ILUMINAÇÃO DE BLINN-PHONG
-    // Half-vector: vetor do meio do caminho entre v e l
-    vec4 h = normalize(v + l);
-    vec3 blinn_phong_specular_term = Ks * I * pow(max(0, dot(n, h)), q);
-
-    // Equação da iluminação de Blinn-Phong
-    vec3 blinn_phong_shading = lambert_diffuse_term + ambient_term + blinn_phong_specular_term;
-    
-    if (cos_beta < cos_alpha){
-        color.rgb = ambient_term;
-    }
+    if(interp_Gouraud){
+        color = color_interp;
+    }   
     else{
-        switch(illumination_model){
-            case LAMBERT:
-                color.rgb = lambert_shading;
-                break;
-            case PHONG:
-                color.rgb = phong_shading;
-                break;
-            case BLINN_PHONG:
-                color.rgb = blinn_phong_shading;
-                break;  
+        // Coordenadas de textura U e V
+        float U = 0.0;
+        float V = 0.0;
+
+        // Aqui usamos o tipo do objeto. Possivelmente podemos ir passando
+        // outros atributos para o shader, como por exemplo o tipo de interpolação de iluminação
+
+        // Objetos genéricos (falta de um nome melhor) usam as coordenadas
+        // de textura do objeto
+        if ( object_type == GENERIC_OBJECT){
+            U = texcoords.x;
+            V = texcoords.y;
         }
+        // Meio que um placeholder, mas mapeia textura com coordenadas esféricas
+        else if ( object_type == SPHERICAL_OBJECT){
+
+            // Cálculo de texturas com coordenadas esféricas
+            vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
+            vec4 p_vec = position_model - bbox_center;
+            vec4 p_hat = bbox_center + (p_vec / length(p_vec));
+            
+            float phi = asin(p_hat[1]);
+            float theta = atan(p_hat[0],p_hat[2]);
+            U = (theta + M_PI) / (2 * M_PI);
+            V = (phi + M_PI_2) / M_PI;
+        }
+
+        // Obtemos a posição da câmera utilizando a inversa da matriz que define o
+        // sistema de coordenadas da câmera.
+            // Obtemos a posição da câmera utilizando a inversa da matriz que define o
+        // sistema de coordenadas da câmera.
+        vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+        vec4 camera_position = inverse(view) * origin;
+
+        // O fragmento atual é coberto por um ponto que percente à superfície de um
+        // dos objetos virtuais da cena. Este ponto, p, possui uma posição no
+        // sistema de coordenadas global (World coordinates). Esta posição é obtida
+        // através da interpolação, feita pelo rasterizador, da posição de cada
+        // vértice.
+        vec4 p = position_world;
+
+        // Normal do fragmento atual, interpolada pelo rasterizador a partir das
+        // normais de cada vértice.
+        vec4 n = normalize(normal);    
+
+        // Vetor que define o sentido da câmera em relação ao ponto atual.
+        vec4 v = normalize(camera_position - p);
+
+        // Posição da fonte de luz
+        vec4 l = light_position - p;
+        
+        const float epsilon = 1e-5;    
+        float cos_beta = dot(normalize(light_direction), normalize(p - light_position));
+        float cos_alpha = cos(light_aperture_angle);
+
+        /*
+            MODELOS DE ILUMINAÇÃO
+        */
+
+        vec3 Kd = Kd0 * texture(diffMap, vec2(U,V) * object_texture_scale).rgb;
+        vec3 Ka = Ka0;
+        vec3 Ks = Ks0; // Refletância especular do objeto
+        float q = ns; // Expoente especular para o modelo de iluminação de Phong
+        vec3 final_color;
+        
+        // Espectro da fonte de iluminação
+        vec3 I = vec3(1.0,1.0,1.0); 
+        
+        // ILUMINAÇÃO DE LAMBERT (DIFUSA)
+        // Termo difuso de Lambert
+        vec3 lambert_diffuse_term = Kd * I * max(0, dot(n, l)); 
+
+        // Equação da iluminação de lambert
+        vec3 lambert_shading = lambert_diffuse_term;
+
+        // ILUMINAÇÃO DE PHONG 
+        // Espectro da luz ambiente
+        vec3 Ia = vec3(0.2,0.2,0.2);
+        // Termo ambiente 
+        vec3 ambient_term = Ka * Ia; 
+        // Vetor que define o sentido da reflexão especular ideal.
+        vec4 r = -l + 2 * n * dot(n, l);
+        // Termo especular utilizando o modelo de iluminação de Phong
+        vec3 phong_specular_term  = Ks * I * pow(max(0, dot(r, v)), q); 
+
+        // Equação da iluminação de Phong
+        vec3 phong_shading = lambert_diffuse_term + ambient_term + phong_specular_term;
+
+        // ILUMINAÇÃO DE BLINN-PHONG
+        // Half-vector: vetor do meio do caminho entre v e l
+        vec4 h = normalize(v + l);
+        vec3 blinn_phong_specular_term = Ks * I * pow(max(0, dot(n, h)), q);
+
+        // Equação da iluminação de Blinn-Phong
+        vec3 blinn_phong_shading = lambert_diffuse_term + ambient_term + blinn_phong_specular_term;
+        
+        if (cos_beta < cos_alpha){
+            color.rgb = ambient_term;
+        }
+        else{
+            switch(illumination_model){
+                case LAMBERT:
+                    color.rgb = lambert_shading;
+                    break;
+                case PHONG:
+                    color.rgb = phong_shading;
+                    break;
+                case BLINN_PHONG:
+                    color.rgb = blinn_phong_shading;
+                    break;  
+            }
+        }
+
+        // NOTE: Se você quiser fazer o rendering de objetos transparentes, é
+        // necessário:
+        // 1) Habilitar a operação de "blending" de OpenGL logo antes de realizar o
+        //    desenho dos objetos transparentes, com os comandos abaixo no código C++:
+        //      glEnable(GL_BLEND);
+        //      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        // 2) Realizar o desenho de todos objetos transparentes *após* ter desenhado
+        //    todos os objetos opacos; e
+        // 3) Realizar o desenho de objetos transparentes ordenados de acordo com
+        //    suas distâncias para a câmera (desenhando primeiro objetos
+        //    transparentes que estão mais longe da câmera).
+        // Alpha default = 1 = 100% opaco = 0% transparente
+        color.a = 1;
+
+        // Cor final com correção gamma, considerando monitor sRGB.
+        // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
+        color.rgb = pow(color.rgb, vec3(1.0,1.0,1.0)/2.2);
     }
-
-    // NOTE: Se você quiser fazer o rendering de objetos transparentes, é
-    // necessário:
-    // 1) Habilitar a operação de "blending" de OpenGL logo antes de realizar o
-    //    desenho dos objetos transparentes, com os comandos abaixo no código C++:
-    //      glEnable(GL_BLEND);
-    //      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    // 2) Realizar o desenho de todos objetos transparentes *após* ter desenhado
-    //    todos os objetos opacos; e
-    // 3) Realizar o desenho de objetos transparentes ordenados de acordo com
-    //    suas distâncias para a câmera (desenhando primeiro objetos
-    //    transparentes que estão mais longe da câmera).
-    // Alpha default = 1 = 100% opaco = 0% transparente
-    color.a = 1;
-
-    // Cor final com correção gamma, considerando monitor sRGB.
-    // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
-    color.rgb = pow(color.rgb, vec3(1.0,1.0,1.0)/2.2);
 }
 
--- src/shader_vertex.glsl
+++ src/shader_vertex.glsl
@@ -20,6 +20,36 @@ out vec4 position_model;
 out vec4 normal;
 out vec2 texcoords;
 
+/*
+
+    MESMA COISAS QUE NO FRAGMENT, USADAS PARA GOURAUD
+
+*/
+
+uniform float object_texture_scale;
+#define GENERIC_OBJECT 0
+#define SPHERICAL_OBJECT 1
+uniform int object_type;
+uniform vec4 bbox_min;
+uniform vec4 bbox_max;
+uniform vec3 Kd0;
+uniform vec3 Ka0;
+uniform vec3 Ks0;
+uniform float ns;
+uniform sampler2D diffMap;
+uniform vec4 light_position;
+uniform vec4 light_direction;
+uniform float light_aperture_angle;
+uniform int illumination_model;
+#define M_PI   3.14159265358979323846
+#define M_PI_2 1.57079632679489661923
+#define LAMBERT 0
+#define PHONG 1
+#define BLINN_PHONG 2
+out vec4 color_interp;
+uniform bool interp_Gouraud;
+
+
 void main()
 {
     // A variável gl_Position define a posição final de cada vértice
@@ -63,5 +93,87 @@ void main()
 
     // Coordenadas de textura obtidas do arquivo OBJ (se existirem!)
     texcoords = texture_coefficients;
+
+
+    /*
+
+        Checa se realizamos interpolação de Gouraud.
+
+    */
+    if(interp_Gouraud){
+        float U = 0.0;
+        float V = 0.0;
+        if ( object_type == GENERIC_OBJECT){
+            U = texcoords.x;
+            V = texcoords.y;
+        }
+        else if ( object_type == SPHERICAL_OBJECT){
+            vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
+            vec4 p_vec = position_model - bbox_center;
+            vec4 p_hat = bbox_center + (p_vec / length(p_vec));
+            
+            float phi = asin(p_hat[1]);
+            float theta = atan(p_hat[0],p_hat[2]);
+            U = (theta + M_PI) / (2 * M_PI);
+            V = (phi + M_PI_2) / M_PI;
+        }
+        vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+        vec4 camera_position = inverse(view) * origin;
+        vec4 p = position_world;
+        vec4 n = normalize(normal);    
+        vec4 v = normalize(camera_position - p);
+        vec4 l = light_position - p;
+        
+        const float epsilon = 1e-5;    
+        float cos_beta = dot(normalize(light_direction), normalize(p - light_position));
+        float cos_alpha = cos(light_aperture_angle);
+
+        vec3 Kd = Kd0 * texture(diffMap, vec2(U,V) * object_texture_scale).rgb;
+        vec3 Ka = Ka0;
+        vec3 Ks = Ks0; // Refletância especular do objeto
+        float q = ns; // Expoente especular para o modelo de iluminação de Phong
+        vec3 final_color;
+        
+        vec3 I = vec3(1.0,1.0,1.0); 
+        
+        vec3 lambert_diffuse_term = Kd * I * max(0, dot(n, l)); 
+        vec3 lambert_shading = lambert_diffuse_term;
+        vec3 Ia = vec3(0.2,0.2,0.2);
+        vec3 ambient_term = Ka * Ia; 
+        vec4 r = -l + 2 * n * dot(n, l);
+        vec3 phong_specular_term  = Ks * I * pow(max(0, dot(r, v)), q); 
+        vec3 phong_shading = lambert_diffuse_term + ambient_term + phong_specular_term;
+        vec4 h = normalize(v + l);
+        vec3 blinn_phong_specular_term = Ks * I * pow(max(0, dot(n, h)), q);
+        vec3 blinn_phong_shading = lambert_diffuse_term + ambient_term + blinn_phong_specular_term;
+        
+        if (cos_beta < cos_alpha){
+            color_interp.rgb = ambient_term;
+        }
+        else{
+            switch(illumination_model){
+                case LAMBERT:
+                    color_interp.rgb = lambert_shading;
+                    break;
+                case PHONG:
+                    color_interp.rgb = phong_shading;
+                    break;
+                case BLINN_PHONG:
+                    color_interp.rgb = blinn_phong_shading;
+                    break;  
+            }
+        }
+        color_interp.a = 1;
+        color_interp.rgb = pow(color_interp.rgb, vec3(1.0,1.0,1.0)/2.2);
+    }
+
+
+
+
+
+
+
+
+
 }
 
